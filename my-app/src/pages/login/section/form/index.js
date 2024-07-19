36a03ff35d32bf79bbfa191a4055afdb46408110
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-07-19 07:40:16 +0530

    login component

--- my-app/src/pages/login/section/form/index.js
+++ my-app/src/pages/login/section/form/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import styles from './form.module.scss';
 import BrandLogo from '../../../../components/shared/brand';
+import {Icon} from '@iconify/react';
 function Form(){
     return (
         <section className={styles["form-container"]}>
            
             <BrandLogo />
+            <div className={styles.form}>
+            <article>
+            <Icon icon={"ri:google-fill"} />
+            <h3>Join with Google</h3>
+            </article>
+            <div className={styles.option}>
+            or join anonymously
+            </div>
+            </div>
         </section>
     );
     
